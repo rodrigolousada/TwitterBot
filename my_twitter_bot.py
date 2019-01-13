ac25c303cf59e0f12b285e6d7f917d7044ba0a19
rodrigolousada <rodrigo.m.lousada@gmail.com>
2019-01-13 04:09:36 +0000

    twitter bot done

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -1,7 +1,8 @@
 import tweepy
+import time
 from keys import *
 
-print('this is my twitter bot')
+print('this is my twitter bot', flush=True)
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -14,8 +15,35 @@ mentions = api.mentions_timeline()
 # print("Last mention id: ", mentions[0].id)
 # print("Type of last last mention id: ", type(mentions[0].id))
 
-for mention in mentions:
-    print(str(mention.id) + ' - ' + mention.text)
-    if '@guiguifonfon' in mention.text.lower():
-        print("found @guiguifonfon!")
-        print("responding back...")
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweets():
+    print("retrieving and replying to tweets...")
+    #DEV NOTE: for TEST purpose use 376831330887036928 in last_seen_id.txt
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(last_seen_id, tweet_mode="extended")
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text, flush=True)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        if '#helloworld' in mention.full_text.lower():
+            print("found #helloworld!", flush=True)
+            print("responding back...", flush=True)
+            api.update_status('@' + mention.user.screen_name 
+                                    + ' #HelloWorld back to you!', mention.id)
+
+while True:
+    reply_to_tweets()
+    time.sleep(15)
